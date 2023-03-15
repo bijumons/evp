b675744498486eebe9e1b7a6db1e3e492bfc4078
biju <biju@ubuntu.myguest.virtualbox.org>
2023-03-15 14:25:27 +0530

    nginx chnage

--- nginx.sh
+++ nginx.sh
@@ -8,7 +8,7 @@ sudo apt-get install nginx
 sudo service nginx stop
 
 # Copy the Nginx configuration file to the appropriate directory
-sudo cp nginx.conf /etc/nginx/sites-available/evp
+sudo cp ./nginx.conf /etc/nginx/sites-available/evp
 
 # Create a symbolic link to enable the configuration file
 sudo ln -s /etc/nginx/sites-available/evp /etc/nginx/sites-enabled/
